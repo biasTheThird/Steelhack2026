683b535f106942ff624c947ca07d864631d2cbfd
galaxriel <vikhasiniviknesh@gmail.com>
2026-09-19 15:43:37 -0400

    added mask

--- Main.java
+++ Main.java
@@ -1,24 +1,24 @@
-import dataAggregregation.*;
-import src.*;
-import src.FixDimensions;
+import dataAggregregation.DefinePixels;
+import dataAggregregation.TargetMaskAssigner;
+import src.Pixel;
 
-import static src.Util.*;
-
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.List;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedImage sourceImage = ImageIO.read(new File("./SourceImages/homer.jpg"));
+        DefinePixels sourceLoader = new DefinePixels(sourceImage);
+        List<Pixel> sourcePixels = sourceLoader.getPixels();
 
-    public static void main(String[] args) {
-        g = new Grapher();
-        g = g.setWinDims(new Point(400, 400));
-        g.setTitle("Now You See Me");
+        BufferedImage targetMask = ImageIO.read(new File("./TargetImage/6aaede0902432_download-modified.jpg"));
+        TargetMaskAssigner.assignTargets(sourcePixels, targetMask);
 
-
-
-        DefinePixels definer = new DefinePixels(FixDimensions.resize(homerPath));
-        g.updateVisual();
+        for (int i = 0; i < Math.min(5, sourcePixels.size()); i++) {
+            Pixel p = sourcePixels.get(i);
+            System.out.println("pixel " + i + " -> targ = (" + p.xTarg + ", " + p.yTarg + ")");
+        }
     }
-
-
 }
