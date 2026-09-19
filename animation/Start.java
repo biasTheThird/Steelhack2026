eacfe691e01620f6f2322be1c70d2f9ef2c346b9
RipZig-33 <romangiordano33@gmail.com>
2026-09-19 14:20:45 -0400

    Grapher update
    Grapher is compatible with Pixel instead of Point

--- animation/Start.java
+++ animation/Start.java
@@ -2,6 +2,21 @@ package animation;
 
 import src.*;
 
+import java.util.ArrayList;
+
 public class Start {
 
+    public static ArrayList<Pixel> pixels;
+
+    public void start() {
+
+
+        /*
+
+        time loop:
+        update pixels
+        draw pixels
+
+        */
+    }
 }
--- src/Grapher.java
+++ src/Grapher.java
@@ -4,8 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
-import java.util.ArrayList;
 import java.util.Arrays;
+
+import static animation.Start.pixels;
 
 /**
  * Each instance of this class can open a window that can display data
@@ -26,13 +27,7 @@ public class Grapher {
     private Point upperBounds = new Point(900, 900);
     private Dimension winDims = new Dimension(900, 900);
     private int backgroundColor = Util.BLACK;
-    private int pointColor = Util.RED;
     private int pointSize = 1;
-
-    private ArrayList<Point[]> dataList = new ArrayList<>(0);
-    private ArrayList<int[]> dataColorList = new ArrayList<>(0);
-    private ArrayList<int[]> dataSizeList = new ArrayList<>(0);
-    private int dataLength = 0;
 
 
     /**
@@ -54,49 +49,12 @@ public class Grapher {
         updateVisual();
     }
 
-    private Grapher(Dimension winDims, ArrayList<Point[]> dataList,
-                   ArrayList<int[]> dataColorList, ArrayList<int[]> dataSizeList) {
+    private Grapher(Dimension winDims) {
         this.winDims = winDims;
         this.upperBounds =  new Point(winDims.width, winDims.height);
-        this.dataList = dataList;
-        this.dataColorList = dataColorList;
-        this.dataSizeList = dataSizeList;
         windowSetup();
         show();
         updateVisual();
-    }
-
-    public void setDataAppearance(int index, int[] color, int[] size) {
-        dataColorList.set(index, color);
-        dataSizeList.set(index, size);
-        updateVisual();
-    }
-    public void setDataAppearance(int index, int color, int size) {
-        int length = dataList.get(index).length;
-        int[] colorArray = new int[length];
-        int[] sizeArray = new int[length];
-
-        for(int i = 0; i < length; i++) {
-            colorArray[i] = color;
-            sizeArray[i] = size;
-        }
-        setDataAppearance(index, colorArray, sizeArray);
-    }
-
-    public int getDataLength() {
-        return dataLength;
-    }
-
-    public int[] getDataColor(int index) {
-        return dataColorList.get(index);
-    }
-
-    public int[] getDataSize(int index) {
-        return dataSizeList.get(index);
-    }
-
-    public Point[] getData(int index) {
-        return dataList.get(index);
     }
 
     public String getTitle() {
@@ -113,15 +71,6 @@ public class Grapher {
 
     public void setPointSize(int pointSize) {
         this.pointSize = pointSize;
-        updateVisual();
-    }
-
-    public int getPointColor() {
-        return pointColor;
-    }
-
-    public void setPointColor(int pointColor) {
-        this.pointColor = pointColor;
         updateVisual();
     }
 
@@ -169,106 +118,6 @@ public class Grapher {
     }
 
     /**
-     * Removes all the data at <b>index</b>
-     * @return if the index is contained in the data
-     */
-    public boolean removeData(int index) {
-        if(index >= dataList.size()) return false;
-        dataList.remove(index);
-        dataColorList.remove(index);
-        dataSizeList.remove(index);
-        dataLength = dataList.size();
-        return true;
-    }
-
-    /**
-     * Removes all the data for <b>points</b>
-     * @return if the <b>points</b> are contained in the data
-     */
-    public boolean removeData(Point[] points) {
-        int index = dataList.indexOf(points);
-        if(index == -1) return false;
-        removeData(index);
-        return true;
-    }
-
-    /**
-     * Removes all the data for <b>colors</b>
-     * @return if the <b>colors</b> are contained in the data
-     */
-    public boolean removeData(int[] colors) {
-        int index = dataColorList.indexOf(colors);
-        if(index == -1) return false;
-        removeData(index);
-        return true;
-    }
-
-    /**
-     * Adds <b>data</b> to the data ArrayList then sets the pixel colors
-     * @return the index in the data ArrayList where <b>data</b> has been stored
-     */
-    public int addData(Point[] data, int[] color, int[] size) {
-        dataList.add(data);
-        dataColorList.add(color);
-        dataSizeList.add(size);
-        dataLength = dataList.size();
-        updateVisual();
-        return dataLength - 1;
-    }
-    /**
-     * Adds <b>data</b> to the data ArrayList then sets the pixel colors
-     * @return the index in the data ArrayList where <b>data</b> has been stored
-     */
-    public int addData(Point[] data, int[] color) {
-        int[] sizeArray = new int[data.length];
-        for(int i = 0; i < data.length; i++) {
-            sizeArray[i] = pointSize;
-        }
-        return addData(data, color, sizeArray);
-    }
-    /**
-     * Adds <b>data</b> to the data ArrayList then sets the pixel colors
-     * @return the index in the data ArrayList where <b>data</b> has been stored
-     */
-    public int addData(Point[] data, int color, int size) {
-        int[] colorArray = new int[data.length];
-        int[] sizeArray = new int[data.length];
-        for(int i = 0; i < data.length; i++) {
-            colorArray[i] = color;
-            sizeArray[i] = size;
-        }
-        return addData(data, colorArray, sizeArray);
-    }
-    /**
-     * Adds <b>data</b> to the data ArrayList then sets the pixel colors
-     * @return the index in the data ArrayList where <b>data</b> has been stored
-     */
-    public int addData(Point data, int color, int size) {
-        return addData(new Point[] {data}, new int[] {color}, new int[] {size});
-    }
-    /**
-     * Adds <b>data</b> to the data ArrayList then sets the pixel colors
-     * @return the index in the data ArrayList where <b>data</b> has been stored
-     */
-    public int addData(Point[] data, int color) {
-        return addData(data, color, pointSize);
-    }
-    /**
-     * Adds <b>data</b> to the data ArrayList then sets the pixel colors
-     * @return the index in the data ArrayList where <b>data</b> has been stored
-     */
-    public int addData(Point data, int color) {
-        return addData(new Point[] {data}, new int[] {color}, new int[] {pointSize});
-    }
-    /**
-     * Adds <b>data</b> to the data ArrayList then sets the pixel colors
-     * @return the index in the data ArrayList where <b>data</b> has been stored
-     */
-    public int addData(Point... data) {
-        return addData(data, pointColor, pointSize);
-    }
-
-    /**
      * Sets up the window by creating a BufferedImage, JFrame, Canvas, and BufferStrategy
      */
     private void windowSetup() {
@@ -312,7 +161,7 @@ public class Grapher {
      */
     public Grapher setWinDims(Dimension windowDims) {
         frame.dispose();
-        return new Grapher(windowDims, dataList, dataColorList, dataSizeList);
+        return new Grapher(windowDims);
     }
     /**
      * Sets the pixel dimensions of the window
@@ -337,9 +186,6 @@ public class Grapher {
                 color, size
         );
     }
-    void setPixel(Point p, int color, int size) {
-        setPixel(p.getX(), p.getY(), color, size);
-    }
 
     /**
      * Sets one pixel's color at (<b>x</b>, <b>y</b>). These coordinates do not map
@@ -363,13 +209,9 @@ public class Grapher {
         //sets the background
         Arrays.fill(pixel, backgroundColor);
         //updates each pixel
-        for(int d = 0; d < dataList.size(); d++) {
-            Point[] data = dataList.get(d);
-            int[] color = dataColorList.get(d);
-            int[] size = dataSizeList.get(d);
-            for(int p = 0; p < data.length; p++) {
-                setPixel(data[p], color[p], size[p]);
-            }
+        for(int i = 0; i < pixels.size(); i++) {
+            Pixel p = pixels.get(i);
+            setPixel(p.xPos, p.yPos, Util.rgb(p.r, p.g, p.b), pointSize);
         }
         //draws the pixels on the window
         Graphics graphics = bs.getDrawGraphics();
