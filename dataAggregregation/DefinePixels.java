213f3e7db76f0ea83224d5a33ad55ad5b3912ccb
biasTheThird <littleclover7@outlook.com>
2026-09-19 13:15:36 -0400

    Added homer

--- dataAggregregation/DefinePixels.java
+++ dataAggregregation/DefinePixels.java
@@ -1,5 +1,13 @@
 package dataAggregregation;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DefinePixels {
-    
+    BufferedImage img = ImageIO.read(new File("homer.jpg"));
+    int height = img.getHeight();
+    int width = img.getWidth();
+
 }
