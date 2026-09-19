39094b996bcf059f8341387efa5b3bc2abb1fb78
biasTheThird <littleclover7@outlook.com>
2026-09-19 15:17:02 -0400

    Resize + pixel properties done!!

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 import dataAggregregation.*;
 import src.*;
+import src.FixDimensions;
 
 import static src.Util.*;
 
+import java.awt.image.BufferedImage;
 import java.util.List;
 
 public class Main {
@@ -12,18 +14,11 @@ public class Main {
         g = g.setWinDims(new Point(400, 400));
         g.setTitle("Now You See Me");
 
-        DefinePixels definer = new DefinePixels(homerPath);
+
+
+        DefinePixels definer = new DefinePixels(FixDimensions.resize(homerPath));
         g.updateVisual();
     }
 
-    public void fillHomer() {
-        DefinePixels definer = new DefinePixels(homerPath);
-        List<Pixel> pixelList = definer.getPixels();
-        for(int y = 0; y < definer.getHeight(); y++) {
-            for(int x = 0; x < definer.getHeight(); x++) {
 
-            }
-        }
-
-    }
 }
